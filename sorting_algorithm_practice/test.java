cd2e6140800570115975b8ca2865703637be35c5
irsad <mdsahilirsad@gmail.com>
2023-01-26 19:45:50 +0530

    practice

--- sorting_algorithm_practice/test.java
+++ sorting_algorithm_practice/test.java
@@ -1,19 +1,54 @@
 package sorting_algorithm_practice;
 
 public class test{
-    public static void main(String[] args) {
-        int n=5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n-1; j++) {
-                System.out.print(" ");
+    public static void bubbleSort(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j]>arr[j+1]){
+                    int swap=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=swap;
+                }
             }
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j+" ");
-            }
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j+" ");
-            }
-            System.out.println();
         }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            int minPos=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[minPos]>arr[j]){
+                    minPos=j;
+                }
+            }
+            int swap=arr[minPos];
+            arr[minPos]=arr[i];
+            arr[i]=swap;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void insertionSort(int arr[]){
+        for (int i = 1; i < arr.length; i++) {
+            int curr=arr[i];
+            int prev=i-1;
+            while (prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,3,6,7,4};
+//        bubbleSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
     }
 }
